b797ad56be6acef9e3ed7cba520dfe5d78781156
hhiiffzzii <93045470+hhiiffzzii@users.noreply.github.com>
2023-03-11 21:07:07 +0800

    Add files via upload

---
+++ task1-Python/modul2/04. input_abs.py
@@ -0,0 +1,11 @@
+# Fungsi abs() statis
+a = -5
+
+c = abs(a)
+print(c)
+
+# Fungsi abs() dinamis
+a =  int(input("Masukkan Nilai A : "))
+
+c = abs(a)
+print(c)
---
+++ task1-Python/modul2/06. input_sqrt.py
@@ -0,0 +1,11 @@
+# Fungsi sqrt() statis
+import math
+
+c = math.sqrt(36)
+print(c)
+
+# Fungsi sqrt() dinamis
+a =  input("Masukkan Nilai : ")
+
+c = math.sqrt(int(a))
+print(c)
