85748d84411d452f9064f527c6ce7e8f0413ea21
hhiiffzzii <93045470+hhiiffzzii@users.noreply.github.com>
2023-03-11 21:02:12 +0800

    Add files via upload

---
+++ task1-Python/modul2/2. input1.py
@@ -0,0 +1,26 @@
+# Contoh kode input
+a =  int(input("Masukkan Nilai A : "))
+b =  int(input("Masukkan Nilai B : "))
+
+print(a,b)
+
+# Contoh kode input integer tanpa fungsi int()
+a =  input("Masukkan Nilai A : ")
+b =  input("Masukkan Nilai B : ")
+
+c = a + b
+print(c)
+
+# Fungsi int() cara pertama
+a =  int(input("Masukkan Nilai A : "))
+b =  int(input("Masukkan Nilai B : "))
+
+c = a + b
+print(c)
+
+# Fungsi int() cara kedua
+a =  input("Masukkan Nilai A : ")
+b =  input("Masukkan Nilai B : ")
+
+c = int(a) + int(b)
+print(c)
